920042300a0d817ad4aec04f57601c86afb5cf8b
CorneliusKibetDC <kapslogo18@gmail.com>
2024-09-29 19:29:26 +0300

    finish challenge

--- Speen_Detector.js
+++ Speen_Detector.js
@@ -1,13 +1,15 @@
-const read_input_output = require('readline');
+const read_input_output = require('readline');//importing readline module
 const ri = read_input_output.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//prompting user to input speed
 ri.question("Input the speed value:", function(speed){
     speed=Number(speed);
     const speedlimit = 70;
   const demeritpointgap = 5;
   const maxNumDemeritPoints = 12;
+  //checking speed and giving the response
   if (speed < speedlimit) {
     console.log("Ok");
   } else {
@@ -17,6 +19,6 @@ ri.question("Input the speed value:", function(speed){
     if (TotalDemeritPoints > maxNumDemeritPoints) {
       console.log("License suspended");
     }
-  }
+    //closing the interface for input
   ri.close();
 });
--- Student_Grade_Generator.js
+++ Student_Grade_Generator.js
@@ -3,12 +3,14 @@ const ri = read_input_output.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+//prompt user to input grade
 ri.question("Enter the student's marks (marks must be between 0 and 100 to get valid grade): ", function(marks) {
+    //checking the marks and determining whast grade
   if (marks >= 0 && marks <= 100) {
     if (marks > 79) {
       console.log(" Your grade is A");
     } else if (marks >= 60 && marks <= 79) {
-      console.log("Your G=grade is B");
+      console.log("Your grade is B");
     } else if (marks >= 49 && marks <= 59) {
       console.log("Your grade is C");
     } else if (marks >= 40 && marks <= 49) {
@@ -17,6 +19,6 @@ ri.question("Enter the student's marks (marks must be between 0 and 100 to get v
       console.log("Your grade is E");
     }
   }
-
+//closing the readkine module and interface for inputs
   ri.close();
 });
